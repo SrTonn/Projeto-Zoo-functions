76be375bc51fc344520e57088471d2d657c0a76e
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-06 17:45:34 -0300

    requisito 06 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
+
+function setType(age) {
+  let type = 'adult';
+  if (age < 18) type = 'child';
+  if (age >= 50) type = 'senior';
+  return type;
+}
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (!Array.isArray(entrants)) return 0;
+  return entrants.reduce((acc, { age }) => {
+    const type = setType(age);
+    return { ...acc, [type]: acc[type] + 1 || 1 };
+  }, {});
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  return Object.entries(countEntrants(entrants))
+    .reduce((acc, [type, quant]) => acc + prices[type] * quant, 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
