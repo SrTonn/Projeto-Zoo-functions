c8e0fb917f0c689cdaa9fc1999311f84dab18548
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-06 16:47:54 -0300

    requisito 05 concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,14 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const animalsObj = species.reduce((acc, obj) => (
+    { ...acc, [obj.name]: obj.residents.length }),
+  {});
+  if (!animal) return animalsObj;
+  if (Object.keys(animal).length === 1) return animalsObj[animal.specie];
+  const specieFound = species.find((specie) => specie.name === animal.specie);
+  return specieFound.residents.filter((sex) => sex.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
+// https://stackoverflow.com/questions/4215737/convert-array-to-object
