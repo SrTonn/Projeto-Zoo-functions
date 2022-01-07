c31ba444b8331e5e56214790dde59a52f5e12c6a
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-07 16:21:43 -0300

    requisito 07 concluido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,27 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+function setOptions(residents, options) {
+  let residentsList = options.sex
+    ? residents.filter(({ sex }) => sex === options.sex) : residents;
+  residentsList = residentsList.map((v) => v.name);
+  if (options && options.sorted) residentsList.sort();
+  return residentsList;
+}
 
 function getAnimalMap(options) {
-  // seu código aqui
+  const animalMap = species.reduce((acc, { name, location, residents }) => {
+    if (!options || !options.includeNames) {
+      return { ...acc, [location]: [...acc[location] || '', name] };
+    }
+
+    const residentsList = setOptions(residents, options);
+    return { ...acc,
+      [location]: [...acc[location] || '', {
+        [name]: residentsList,
+      }] };
+  }, {});
+
+  return animalMap;
 }
 
 module.exports = getAnimalMap;
