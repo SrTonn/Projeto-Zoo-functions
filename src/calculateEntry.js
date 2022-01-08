48d70f47d30ba53e34c4828d0d5aa301049c4723
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-08 09:44:00 -0300

    requisito 06 refatorado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,21 +1,16 @@
 const { prices } = require('../data/zoo_data');
 
-function setType(age) {
-  let type = 'adult';
-  if (age < 18) type = 'child';
-  if (age >= 50) type = 'senior';
-  return type;
-}
-
 function countEntrants(entrants) {
-  if (!Array.isArray(entrants)) return 0;
   return entrants.reduce((acc, { age }) => {
-    const type = setType(age);
+    let type = 'adult';
+    if (age < 18) type = 'child';
+    if (age >= 50) type = 'senior';
     return { ...acc, [type]: acc[type] + 1 || 1 };
   }, {});
 }
 
 function calculateEntry(entrants) {
+  if (!Array.isArray(entrants)) return 0;
   return Object.entries(countEntrants(entrants))
     .reduce((acc, [type, quant]) => acc + prices[type] * quant, 0);
 }
