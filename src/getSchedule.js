591a2f58935c8e6771d7082bc5b86e849e1ec220
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-08 11:54:13 -0300

    requisito 08 concluido

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,22 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  try {
+    return species.find(({ name }) => name === scheduleTarget).availability;
+  } catch (error) {
+    const schedule = Object.entries(hours).reduce((acc, [day, { open, close }]) =>
+      ({ ...acc, [day]: { officeHour: `Open from ${open}am until ${close}pm` } }),
+    {});
+    Object.keys(schedule).forEach((day) => {
+      const speciesPerDay = species.reduce((acc, v) =>
+        ((v.availability.includes(day) && [...acc, v.name]) || acc), []);
+      schedule[day].exhibition = speciesPerDay;
+    });
+    schedule.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    return schedule[scheduleTarget] ? { [scheduleTarget]: schedule[scheduleTarget] } : schedule;
+  }
 }
+const actual = getSchedule('lions');
+console.log(actual);
 
 module.exports = getSchedule;
