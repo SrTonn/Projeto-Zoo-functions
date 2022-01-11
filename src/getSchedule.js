848e58aa362c4ec1fd74b20aca4a0198ec90f80a
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-11 10:27:47 -0300

    requisito 08 refatorado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,20 +1,18 @@
 const { species, hours } = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
+const getSchedule = (scheduleTarget) => {
   try {
     return species.find(({ name }) => name === scheduleTarget).availability;
   } catch (error) {
     const schedule = Object.entries(hours).reduce((acc, [day, { open, close }]) =>
-      ({ ...acc, [day]: { officeHour: `Open from ${open}am until ${close}pm` } }),
-    {});
+      ({ ...acc, [day]: { officeHour: `Open from ${open}am until ${close}pm` } }), {});
     Object.keys(schedule).forEach((day) => {
-      const speciesPerDay = species.reduce((acc, v) =>
-        ((v.availability.includes(day) && [...acc, v.name]) || acc), []);
-      schedule[day].exhibition = speciesPerDay;
+      schedule[day].exhibition = species.reduce((acc, specie) =>
+        ((specie.availability.includes(day) && [...acc, specie.name]) || acc), []);
     });
     schedule.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
     return schedule[scheduleTarget] ? { [scheduleTarget]: schedule[scheduleTarget] } : schedule;
   }
-}
+};
 
 module.exports = getSchedule;
