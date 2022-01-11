35f56a7c9f39c969e32d16a21b4ac44e239b2370
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-11 10:31:11 -0300

    requisito 04 refatorado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,16 +2,14 @@ const { employees } = require('../data/zoo_data');
 
 const isManager = (id) => employees.some(({ managers }) => managers.includes(id));
 
-function getRelatedEmployees(managerId) {
+const getRelatedEmployees = (managerId) => {
   if (isManager(managerId)) {
-    return employees.reduce((acc, manager) => {
-      if (manager.managers.includes(managerId)) {
-        acc.push(`${manager.firstName} ${manager.lastName}`);
-      }
+    return employees.reduce((acc, { managers, firstName, lastName }) => {
+      if (managers.includes(managerId)) acc.push(`${firstName} ${lastName}`);
       return acc;
     }, []);
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-}
+};
 
 module.exports = { isManager, getRelatedEmployees };
