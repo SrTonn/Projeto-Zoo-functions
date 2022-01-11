ecfd115bbbe6b11b1dabe0cea57417327472531c
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-11 10:44:05 -0300

    requisito 07 refatorado

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,25 +1,18 @@
 const { species } = require('../data/zoo_data');
 
-function setOptions(residents, options) {
-  let residentsList = options.sex
-    ? residents.filter(({ sex }) => sex === options.sex) : residents;
-  residentsList = residentsList.map((v) => v.name);
-  if (options && options.sorted) residentsList.sort();
-  return residentsList;
-}
+const setOptions = (residents, options) => {
+  let residentsList = options.sex ? residents.filter(({ sex }) => sex === options.sex) : residents;
+  residentsList = residentsList.map(({ name }) => name);
+  return options.sorted ? residentsList.sort() : residentsList;
+};
 
-function getAnimalMap(options) {
-  return species.reduce((acc, { name, location, residents }) => {
-    if (!options || !options.includeNames) {
-      return { ...acc, [location]: [...acc[location] || '', name] };
-    }
+const getAnimalMap = (options) => species.reduce((acc, { name, location, residents }) => {
+  if (!options || !options.includeNames) {
+    return { ...acc, [location]: [...acc[location] || '', name] };
+  }
 
-    const residentsList = setOptions(residents, options);
-    return { ...acc,
-      [location]: [...acc[location] || '', {
-        [name]: residentsList,
-      }] };
-  }, {});
-}
+  const residentsList = setOptions(residents, options);
+  return { ...acc, [location]: [...acc[location] || '', { [name]: residentsList }] };
+}, {});
 
 module.exports = getAnimalMap;
