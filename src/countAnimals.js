f48bf4ac8f27a07e0b10578bd2e04f8e5055d82f
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-11 10:57:20 -0300

    requisito 05 refatorado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,14 +1,13 @@
 const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  const animalsObj = species.reduce((acc, obj) => (
-    { ...acc, [obj.name]: obj.residents.length }),
-  {});
+const countAnimals = (animal) => {
+  const animalsObj = species.reduce((acc, { name, residents }) =>
+    ({ ...acc, [name]: residents.length }), {});
   if (!animal) return animalsObj;
   if (Object.keys(animal).length === 1) return animalsObj[animal.specie];
-  const specieFound = species.find((specie) => specie.name === animal.specie);
-  return specieFound.residents.filter((sex) => sex.sex === animal.sex).length;
-}
+  const { residents } = species.find(({ name }) => name === animal.specie);
+  return residents.filter(({ sex }) => sex === animal.sex).length;
+};
 
 module.exports = countAnimals;
 // https://stackoverflow.com/questions/4215737/convert-array-to-object
