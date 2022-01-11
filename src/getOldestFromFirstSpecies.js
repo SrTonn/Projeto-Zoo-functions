d6952fb8fb268d07ee5b69e0312ed65e538c5824
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-11 10:32:57 -0300

    requisito 09 refatorado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,9 +1,9 @@
 const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
+const getOldestFromFirstSpecies = (id) => {
   const specieId = employees.find((obj) => obj.id === id).responsibleFor[0];
   const { residents } = species.find((obj) => obj.id === specieId);
   return Object.values(residents.reduce((acc, obj) => (acc.age > obj.age ? acc : obj)));
-}
+};
 
 module.exports = getOldestFromFirstSpecies;
